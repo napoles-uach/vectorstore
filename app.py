3c12b301b4c1e876c4f835fb985c91b5ac4f4532
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-07-14 17:39:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ else:
 with st.spinner('Creating assistant...'):
     paper_assistant = client.beta.assistants.create(
       name="Paper Assistant",
-      instructions="You are an author of a research paper. Use your knowledge base to answer questions about the research related to molecular packing.",
+      instructions="You are an author of a research paper. Use your knowledge base to answer questions about the research related to molecular packing. Use markdown and latex to show mathematical formulas",
       model="gpt-4o",
       tools=[{"type": "file_search"}],
     )
